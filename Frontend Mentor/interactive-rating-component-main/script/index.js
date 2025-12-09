17e14086ebc4d87ba6a8be586886e2d84005343c
Abasiomofon Udoh <omofon.u@gmail.com>
2025-12-09 05:59:34 +0100

    fix(tailwind): remove package file - index.js assign dom nodes

--- Frontend Mentor/interactive-rating-component-main/script/index.js
+++ Frontend Mentor/interactive-rating-component-main/script/index.js
@@ -1,24 +1,8 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    const ratingCard = document.getElementById("rating-card");
-    const thankYouCard = document.getElementById("thank-you-card")
-    const ratingButtons = document.querySelectorAll(button[role="radio"]);
-    const submitButton = document.querySelector(button[type="submit"]);
-    const selectedRating = null;
+document.addEventListener("DOMContentLoaded", () => {
+  const ratingForm = document.getElementById("rating-form");
+  const ratingButtons = document.querySelectorAll(".round-ctn")
 
-    
-    // Hide Card
-    function hideCard(cardElement) {
-        cardElement.classList.add("hidden");
-    }
-
-    // Reveal Card
-    function showCard(cardElement) {
-        cardElement.classList.remove("hidden")
-    }
-
-    function getRating() {
-        if (!selectedRating) return null;
-
-
-    }
-})
+  const ratingCard = document.getElementById("rating-card")
+  const thankYouCard = document.getElementById("thank-you-card")
+  const selectedRatingSpan = document.getElementById("selected-rating")
+});
